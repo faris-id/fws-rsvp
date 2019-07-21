08460feb5594b56a59d0729c73c8749bfa6ecc0f
Faris Arifiansyah <faris.arifiansyah@gmail.com>
2019-07-21 21:40:48 +0700

    add entity + repo & use case interfaces

---
+++ service.go
@@ -0,0 +1,40 @@
+package rsvp
+
+import (
+	"context"
+	"time"
+)
+
+// Parameter is a struct to simplify passing parameter into function
+type Parameter struct {
+	Limit  int
+	Offset int
+}
+
+// RsvpResult is a struct container to put result
+type RsvpResult struct {
+	Data  []*Rsvp
+	Total int64
+}
+
+// Rsvp Entity
+type Rsvp struct {
+	ID        string    `json:"-" bson:"_id,omitempty"`
+	Name      string    `json:"name" bson:"name"`
+	Address   string    `json:"address" bson:"address"`
+	Attend    uint8     `json:"attend" bson:"attend"`
+	Message   string    `json:"message" bson:"message"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+}
+
+// RsvpRepo provides data interchange between
+// application and data provider.
+type RsvpRepo interface {
+	CreateRsvp(ctx context.Context, rp *Rsvp) error
+	GetRsvps(ctx context.Context, p *Parameter) (*RsvpResult, error)
+}
+
+type Usecase interface {
+	CreateRsvp(ctx context.Context, rp *Rsvp) error
+	GetRsvps(ctx context.Context, p *Parameter) (*RsvpResult, error)
+}
